3bc48ed735758eaa9f115704dc5ed9f66ad17a8f
Riko-oninternet <andreadelaurentis15@gmail.com>
2024-10-15 14:14:56 +0200

    h

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,6 @@ export default function RootLayout({
           <KeyBlocker />
           <CookieConsent />
           {children}
-          <Analytics />
         </CookieProvider>
       </body>
     </html>
