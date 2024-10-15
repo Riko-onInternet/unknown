2f7ef2b99d9981a2ec4e959044ac6a499a20cfc5
Riko-oninternet <andreadelaurentis15@gmail.com>
2024-10-15 14:10:55 +0200

    ffg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import "@/assets/css/globals.css";
 import "@/assets/css/font.css";
 
-import { Analytics } from "@vercel/analytics/react"
-
 import CookieConsent from "@/assets/components/CookieConsent";
 import { CookieProvider } from "@/assets/components/CookieProvider";
 import KeyBlocker from "@/assets/components/KeyBlock";
