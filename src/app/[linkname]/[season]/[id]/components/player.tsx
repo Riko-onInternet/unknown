aae93e1099246b33e3016b3a603cf55f290068fd
Riko-onInternet <andreadelaurentis15@gmail.com>
2024-11-03 22:52:14 +0100

    ssss

--- src/app/[linkname]/[season]/[id]/components/player.tsx
+++ src/app/[linkname]/[season]/[id]/components/player.tsx
@@ -632,7 +632,7 @@ export default function Player({
         </a>
       </div>
 
-      <p className="w-full h-full flex items-center justify-center text-base">
+      <p className="w-full h-full flex items-center justify-center text-center text-base">
         Stiamo ancora sistemando il server dove mettere gli episodi, vi chiediamo
         di essere pazienti e di aspettare.
       </p>
